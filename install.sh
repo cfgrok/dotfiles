6a71eacde6746adbb11d893d70c70c201f3621df
Ezra Parker <ezra@cfgrok.com>
2017-06-10 17:37:58 -0700

    Update install script
     - Check for presence of ansible PPA before adding & disable prompt
     - Disable prompt for installation of ansible & git
     - Fix issues w/ dotfiles directory check & workspace directory creation

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-sudo apt-get install software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
+if ! grep -qs 'deb http' /etc/apt/sources.list.d/ansible-ubuntu-ansible-xenial.list; then
+    sudo apt-add-repository -y ppa:ansible/ansible
+fi
+
 sudo apt-get update
-sudo apt-get install ansible git
+sudo apt-get install -y ansible git
 
 cd ~
 
-if [ ! -d "~/Documents/workspace/dotfiles" ]; then
-    mkdir Documents/workspace
+if [ ! -d ~/Documents/workspace/dotfiles ]; then
+    mkdir -p Documents/workspace
     cd Documents/workspace
     git clone git@github.com:cfgrok/dotfiles.git
     echo 'primary' > ~/Documents/workspace/dotfiles/.playbook
