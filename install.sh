6a1f1971d31a8a4da5f04dcfbf493213e45f9238
Ezra Parker <ezra@cfgrok.com>
2022-07-24 18:53:19 -0700

    Add task to set all user settings
      - Install custom gsetting ansible module from
        https://github.com/jistr/ansible-gsetting
      - Set user settings with gsetting or dconf

--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
-if ! grep -qs 'deb http' /etc/apt/sources.list.d/ansible-ubuntu-ansible-*.list; then
+if [ ! -f /etc/apt/sources.list.d/ansible-ansible-*.list ]; then
     sudo apt-add-repository -y ppa:ansible/ansible
 fi
 
 sudo apt update
 sudo apt install -y ansible git
+
+module_path="$HOME"/.ansible/plugins/modules
+
+if [ ! -f "$module_path"/gsetting ]; then
+    mkdir -p "$module_path"
+    wget -O "$module_path"/gsetting https://raw.githubusercontent.com/jistr/ansible-gsetting/master/gsetting.py
+fi
 
 if [ ! -d "$HOME"/Documents/workspace/dotfiles ]; then
     cd "$HOME"
