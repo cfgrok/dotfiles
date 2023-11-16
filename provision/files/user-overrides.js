50198f057472ef9addd1e426216408a9b9d793df
Ezra Parker <ezra@cfgrok.com>
2023-11-16 12:27:56 -0800

    Tweaks to Firefox settings
      - Update installed extensions
      - Further refinement of arkenfox settings, including disabling
        resistFingerprinting

--- provision/files/user-overrides.js
+++ provision/files/user-overrides.js
@@ -1,24 +1,21 @@
 user_pref("browser.startup.page", 3); // 0102
-user_pref("network.trr.mode", 2); // 0710
-user_pref("media.eme.enabled", true); // 2022
+user_pref("network.trr.mode", 3); // 0710
 user_pref("privacy.clearOnShutdown.history", false); // 2811
-user_pref("privacy.cpd.history", true);  // 2820
+user_pref("privacy.cpd.history", false);  // 2820
+user_pref("privacy.cpd.offlineApps", true); // 2820
 user_pref("privacy.resistFingerprinting", false); // 4501
 user_pref("privacy.resistFingerprinting.letterboxing", false); // 4504
 user_pref("signon.rememberSignons", false); // 5003
 user_pref("browser.sessionstore.resume_from_crash", false); // 5008
 user_pref("browser.download.forbid_open_with", true); // 5009
 user_pref("browser.urlbar.suggest.topsites", false); // 5010
-user_pref("browser.download.folderList", 2); // 5016
 user_pref("extensions.formautofill.addresses.enabled", false); // 5017
 user_pref("extensions.formautofill.creditCards.enabled", false); // 5017
-user_pref("extensions.formautofill.heuristics.enabled", false); // 5017
-user_pref("browser.newtabpage.activity-stream.feeds.topsites", false); // 9000
-user_pref("browser.newtabpage.activity-stream.showSearch", false); // 9000
+user_pref("browser.download.useDownloadDir", true); // 9000
 user_pref("browser.tabs.loadBookmarksInTabs", true); // 9000
 user_pref("extensions.pocket.enabled", false); // 9000
 user_pref("media.autoplay.default", 5); // 9000
-user_pref("privacy.cpd.cookies", true); // 9000
-user_pref("privacy.cpd.history", false); // 9000
-user_pref("privacy.cpd.offlineApps", true); // 9000
+user_pref("media.eme.enabled", true); // 9000
+//user_pref("privacy.resistFingerprinting.exemptedDomains", "mail.google.com"); // 9000 - experimental feature?
+//user_pref("privacy.fingerprintingProtection.overrides", "+AllTargets,-JSDateTimeUTC"); // 9000 - experimental feature?
 user_pref("_overrides.parrot", "overrides: success");
