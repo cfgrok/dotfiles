417b9775b2648724c77bc3937b81caea08b815df
Ezra Parker <ezra@cfgrok.com>
2024-07-25 11:36:25 -0700

    feat: update Arkenfox overrides

--- playbooks/files/user-overrides.js
+++ playbooks/files/user-overrides.js
@@ -1,8 +1,12 @@
 user_pref("browser.startup.page", 3); // 0102
 user_pref("network.trr.mode", 3); // 0710
+user_pref("browser.download.useDownloadDir", true); // 2651
 user_pref("privacy.clearOnShutdown.history", false); // 2811
-user_pref("privacy.cpd.history", false);  // 2820
-user_pref("privacy.cpd.offlineApps", true); // 2820
+user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", false); // 2811
+user_pref("privacy.clearSiteData.historyFormDataAndDownloads", false); // 2820
+user_pref("privacy.cpd.history", false); // 2830
+user_pref("privacy.cpd.offlineApps", true); // 2830
+user_pref("privacy.clearHistory.historyFormDataAndDownloads", false); // 2830
 user_pref("privacy.resistFingerprinting", false); // 4501
 user_pref("privacy.resistFingerprinting.letterboxing", false); // 4504
 user_pref("signon.rememberSignons", false); // 5003
@@ -11,11 +15,8 @@ user_pref("browser.download.forbid_open_with", true); // 5009
 user_pref("browser.urlbar.suggest.topsites", false); // 5010
 user_pref("extensions.formautofill.addresses.enabled", false); // 5017
 user_pref("extensions.formautofill.creditCards.enabled", false); // 5017
-user_pref("browser.download.useDownloadDir", true); // 9000
+user_pref("media.eme.enabled", true); // 5508
 user_pref("browser.tabs.loadBookmarksInTabs", true); // 9000
 user_pref("extensions.pocket.enabled", false); // 9000
 user_pref("media.autoplay.default", 5); // 9000
-user_pref("media.eme.enabled", true); // 9000
-//user_pref("privacy.resistFingerprinting.exemptedDomains", "mail.google.com"); // 9000 - experimental feature?
-//user_pref("privacy.fingerprintingProtection.overrides", "+AllTargets,-JSDateTimeUTC"); // 9000 - experimental feature?
 user_pref("_overrides.parrot", "overrides: success");
