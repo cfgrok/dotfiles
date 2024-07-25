b8d555686fb8b88ed086753e957672236d8fa631
Ezra Parker <ezra@cfgrok.com>
2024-07-25 14:24:11 -0700

    feat: add debugging task

--- up.sh
+++ up.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd "$HOME"/Documents/workspace/dotfiles/playbooks
-ansible-playbook -vv -K main.yml "$@"
+ansible-playbook -vv -K main.yml $([[ "$@" =~ "-t debug" ]] || printf %s "--skip-tags debug") "$@"
